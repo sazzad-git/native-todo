c848ddf6b19844c40665392ee34020ff201d484b
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2025-05-12 17:02:49 +0600

    active button done

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,12 +1,31 @@
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
 const ActionButton = () => {
   return (
-    <View>
-      <Text>ActionButton</Text>
-    </View>
+    <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+      <FontAwesome6 name="add" size={24} color="black" style={styles.icon} />
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#007AFF",
+    width: 80,
+    height: 80,
+    borderRadius: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 50,
+    right: 24,
+  },
+  icon: {
+    fontSize: 40,
+    color: "white",
+  },
+});
 
 export default ActionButton;
